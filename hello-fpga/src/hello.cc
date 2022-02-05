0844fc504cc5a48dc5be1ea750d9cbe083fe7d35
Scott Thibault <sathibault@yahoo.com>
2022-02-05 12:14:35 -0500

    Add comments to hello-fpga example

--- hello-fpga/src/hello.cc
+++ hello-fpga/src/hello.cc
@@ -1,16 +1,31 @@
 #include <stdio.h>
+
+// Include FPGA programming framework
 #include "core-fusion.h"
 
+// Function to implement on FPGA
 int16_t calc(int16_t x) {
   return 7 * x - 15;
 }
 
 int main(int argc, char *argv[]) {
+  // Create an instance of target FPGA board class
   Upduino board;
+
+  // Transform the calc C function into an FPGA function
+  // The serialIn/serialOut parameters indicate how to communication with the board
+  // from the computer in order to call the FPGA function.
   auto fun = fpga_func(board.serialIn, board.serialOut, calc);
 
+  // Must start the board after all FPGA functions have been created
   board.start(argc, argv);
 
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 5; i++) {
+    // Call the FPGA function
+    // (sends the input over the serialIn port and captures the output from serialOut)
     printf("calc(%d) = %d\n", i, fun(i));
+  }
+
+  // Stop the board before exiting
+  board.stop();
 }
