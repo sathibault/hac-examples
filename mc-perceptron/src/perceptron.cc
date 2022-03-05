8d1bec9dc0c0366b34d4c424dd3a9945c12517fe
Scott Thibault <sathibault@yahoo.com>
2022-03-05 09:31:48 -0500

    Perceptron updates

--- mc-perceptron/src/perceptron.cc
+++ mc-perceptron/src/perceptron.cc
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "core-fusion.h"
 
-// Dot product of a 4-element data vector and 4-element weight vector
-
-void dotproduct(in_array<int16_t,4> features, out_array<int16_t,4> out) {
+// Multi-class perceptron is a matrix/vector multiplication
+void mat_vec_mul(in_array<int16_t,4> features, out_array<int16_t,4> out) {
   int16_t coef[12] = {
     -9,  25, -28, -27,
     -3, -31,  58, -31,
-    -10,   4,  80,  79
+    -10,  4,  80,  79
   }; // weights
 
   out.resize(3); // set the actual output size
@@ -21,19 +20,15 @@ void dotproduct(in_array<int16_t,4> features, out_array<int16_t,4> out) {
 
 int main(int argc, char *argv[]) {
   Upduino board;
-  auto fun = fpga_func(board.serialIn, board.serialOut, dotproduct);
+  auto fun = fpga_func(board.serialIn, board.serialOut, mat_vec_mul);
 
   board.start(argc, argv);
 
-  // Test data: four samples, for inputs each
+  // Test data: 4 example flower, 4 values each
   int16_t data[4][4] = {
-    {-18,  62, -38, -34},
-    {-41,  -5, -43, -38},
-    { -2, -34,   4,   0},
-    { 33,  -5,  26,  46}
+    {-18,  62, -38, -34}, {-41,  -5, -43, -38}, { -2, -34,   4,   0}, { 33,  -5,  26,  46}
   };
-
-  int expected[] = {0, 0, 1, 2};
+  int expected[] = {0, 0, 1, 2}; // Correct flower type of the 4 examples
 
   for (int i = 0; i < 4; i++) {
     int16_t cls[3];
--- perceptron/src/perceptron.cc
+++ perceptron/src/perceptron.cc
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include "core-fusion.h"
 
-// Dot product of a 4-element data vector and 4-element weight vector
-
+// Perceptron is a dot product of a feature vector and a weight vector
 int16_t dotproduct(in_array<int16_t,4> features) {
   int16_t coef[] = { 3, -2,  8, 10 }; // weights
 
-  int16_t acc = 0;
+  int16_t dot = 0;
   for (int i = 0; i < 4; i++)
-    acc += coef[i] * features[i];
+    dot += coef[i] * features[i];
 
-  return acc;
+  return dot;
 }
 
 int main(int argc, char *argv[]) {
@@ -19,16 +18,13 @@ int main(int argc, char *argv[]) {
 
   board.start(argc, argv);
 
-  // Test data: four samples, for inputs each
+  // Test data: 4 example flowers, 4 values each
   int16_t data[4][4] = {
-    {-18,  62, -38, -34},
-    {-41,  -5, -43, -38},
-    { -2, -34,   4,   0},
-    { 33,  -5,  26,  46}
+    {-18,  62, -38, -34}, {-41,  -5, -43, -38}, { -2, -34,   4,   0}, { 33,  -5,  26,  46}
   };
 
   for (int i = 0; i < 4; i++) {
     auto y = fun(as_array(data[i], 4));
-    printf("%d %d\n",i, y);
+    printf("Ex %d: %d\n",i, y);
   }
 }
